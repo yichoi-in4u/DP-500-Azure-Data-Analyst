40c03c6056b0d0b04aeac8cb016e1f736f1bab4f
yichoi-in4u <114711530+yichoi-in4u@users.noreply.github.com>
2023-03-24 15:19:08 +0900

    Update setup.ps1

--- Allfiles/04/setup.ps1
+++ Allfiles/04/setup.ps1
@@ -3,6 +3,40 @@ write-host "Starting script at $(Get-Date)"
 
 Install-Module -Name Az.Synapse
 
+$subs = Get-AzSubscription | Select-Object
+if($subs.GetType().IsArray -and $subs.length -gt 1){
+    Write-Host "You have multiple Azure subscriptions - please select the one you want to use:"
+    for($i = 0; $i -lt $subs.length; $i++)
+    {
+            Write-Host "[$($i)]: $($subs[$i].Name) (ID = $($subs[$i].Id))"
+    }
+    $selectedIndex = -1
+    $selectedValidIndex = 0
+    while ($selectedValidIndex -ne 1)
+    {
+            $enteredValue = Read-Host("Enter 0 to $($subs.Length - 1)")
+            if (-not ([string]::IsNullOrEmpty($enteredValue)))
+            {
+                if ([int]$enteredValue -in (0..$($subs.Length - 1)))
+                {
+                    $selectedIndex = [int]$enteredValue
+                    $selectedValidIndex = 1
+                }
+                else
+                {
+                    Write-Output "Please enter a valid subscription number."
+                }
+            }
+            else
+            {
+                Write-Output "Please enter a valid subscription number."
+            }
+    }
+    $selectedSub = $subs[$selectedIndex].Id
+    Select-AzSubscription -SubscriptionId $selectedSub
+    az account set --subscription $selectedSub
+}
+
 $sqlDatabaseName = "sqldw"
 $sqlUser = "SQLUser"
 
